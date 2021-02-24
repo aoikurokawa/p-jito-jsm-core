088de61605b6cc6c4e9c50187f937a8d9638ecdf
Aoi <ht.so.ik0102@gmail.com>
2021-02-24 07:56:11 +0700

    check git status

--- client/src/component/Balance.js
+++ client/src/component/Balance.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Balance = ({account}) => {
     return (
         <div>
-            <h1>Coin components</h1>
+            <h1>Coin component</h1>
             <p>account: {account}</p>
         </div>
     );
